2583e25b97af58d43b3424aaf1ad1fa661282e54
alex <alex_kobilianskiy@bigmir.net>
2019-08-08 08:41:23 +0300

    bottom-line complete

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
         return false;
     });
 
+    $(".toggle-serv-mnu").click(function() {
+        $(this).toggleClass("on");
+        $(".serv-mobile-mnu").slideToggle();
+        return false;
+    });
+
+    $(".serv-mobile-mnu ul li.menu-item-has-children a").click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass("opened");
+        $(this).parents("li").find('ul').slideToggle();
+        return false;
+    });
+
+    function heightses() {
+
+        if ($(window).width()>480) {
+        }
+
+        $('.serv-mnu>ul>li>a').height('auto').equalHeights();
+    }
+
+    $(window).resize(function() {
+        heightses();
+    });
+    heightses();
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
